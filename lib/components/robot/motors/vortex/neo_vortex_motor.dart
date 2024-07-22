05c2992a889568dd4cd056bc11f2113ad5fc740d
Saathvik k <saathvikkamma@gmail.com>
2024-07-21 22:47:47 -0500

    updated Vortex Motors

--- lib/components/robot/motors/vortex/neo_vortex_motor.dart
+++ lib/components/robot/motors/vortex/neo_vortex_motor.dart
@@ -1,42 +1,47 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/src/consumer.dart';
-// import 'package:freezed_annotation/freezed_annotation.dart';
-// import 'package:player_move/components/robot/motors/motor.dart';
-// part 'neo_vortex_motor.g.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/src/consumer.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:player_move/components/robot/motors/motor.dart';
+part 'neo_vortex_motor.g.dart';
 
-// @JsonSerializable()
-// class VortexMotor extends Motor {
-//   static const String kName = "Vortex";
-//   VortexMotor()
-//       : super(acceleration: 14, maximumSpeed: 10, cost: 10, name: kName);
-//   @override
-//   void updateTotalAcceleration(WidgetRef ref, dynamic constants) {}
+@JsonSerializable(explicitToJson: true)
+class VortexMotor extends Motor {
+  static const double kAcceleration = 14;
+  static const double kMaximumSpeed = 10;
+  static const double kCost = 10;
+  static const String kName = "Vortex";
+  VortexMotor()
+      : super(
+          acceleration: kAcceleration,
+          maximumSpeed: kMaximumSpeed,
+          cost: kCost,
+          name: kName,
+        );
 
-//   @override
-//   void updateTotalMaxSpeed(WidgetRef ref, dynamic constants) {
-//     // TODO: implement updateTotalMaxSpeed
-//   }
+  @override
+  void updateTotalAcceleration(WidgetRef ref, dynamic constants) {}
 
-//   @override
-//   String toString() {
-//     return "Vortex";
-//   }
+  @override
+  void updateTotalMaxSpeed(WidgetRef ref, dynamic constants) {
+    // TODO: implement updateTotalMaxSpeed
+  }
 
-//   static Image toImage(BuildContext context) {
-//     return Image.asset(
-//       "assets/images/NEO_11_motor.png",
-//       height: MediaQuery.of(context).size.shortestSide / 7,
-//       width: MediaQuery.of(context).size.shortestSide / 7,
-//     );
-//   }
+  static Image toImage(BuildContext context) {
+    return Image.asset(
+      "assets/images/neo_vortex_motor.png",
+      height: MediaQuery.of(context).size.shortestSide / 7,
+      width: MediaQuery.of(context).size.shortestSide / 7,
+    );
+  }
 
-//   @override
-//   void updateTotalCost(WidgetRef ref, constants) {
-//     // TODO: implement updateTotalCost
-//   }
+  @override
+  void updateTotalCost(WidgetRef ref, constants) {
+    // TODO: implement updateTotalCost
+  }
 
-//   factory VortexMotor.fromJson(Map<String, dynamic> json) =>
-//       _$VortexMotorFromJson(json);
-//   @override
-//   Map<String, dynamic> toJson() => _$VortexMotorToJson(this);
-// }
+  factory VortexMotor.fromJson(Map<String, dynamic> json) =>
+      _$VortexMotorFromJson(json);
+
+  @override
+  Map<String, dynamic> toJson() => _$VortexMotorToJson(this);
+}
---
+++ lib/components/robot/motors/vortex/neo_vortex_motor.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'neo_vortex_motor.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+VortexMotor _$VortexMotorFromJson(Map<String, dynamic> json) => VortexMotor()
+  ..acceleration = (json['acceleration'] as num).toDouble()
+  ..maximumSpeed = (json['maximumSpeed'] as num).toDouble()
+  ..cost = (json['cost'] as num).toDouble()
+  ..name = json['name'] as String;
+
+Map<String, dynamic> _$VortexMotorToJson(VortexMotor instance) =>
+    <String, dynamic>{
+      'acceleration': instance.acceleration,
+      'maximumSpeed': instance.maximumSpeed,
+      'cost': instance.cost,
+      'name': instance.name,
+    };
